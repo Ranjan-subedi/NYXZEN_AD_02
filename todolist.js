affe37ecb71cd9ad772f202de5677f4c535282c9
Ranjan Subedi <144402172+Ranjan-subedi@users.noreply.github.com>
2024-01-14 14:32:46 +0545

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,100 @@
+const itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
+
+document.querySelector("#enter").addEventListener("click", () => {
+    const itemInput = document.getElementById("items");
+    createItem(itemInput);
+});
+
+function displayItems() {
+    let items = "";
+    for (let i = 0; i < itemsArray.length; i++) {
+        items += `<div class="item">
+            <div class="input-controller">
+                <textarea disabled>${itemsArray[i]}</textarea>
+                <div class="edit-controller">
+                    <i class="fa-solid fa-check deleteBtn"></i>  
+                    <i class="fa-solid fa-pen editBtn"></i>
+                </div>
+            </div>
+            <div class="update-controller">
+                <button class="saveBtn">Save</button>
+                <button class="cancelBtn">Cancel</button>
+            </div>
+        </div>`;
+    }
+    document.querySelector(".to-do-list").innerHTML = items;
+    activateDeleteListeners();
+    activateEditListeners();
+    activateSaveListeners();
+    activateCancelListeners();
+}
+
+function activateDeleteListeners() {
+    let deleteBtn = document.querySelectorAll(".deleteBtn");
+    deleteBtn.forEach((db, i) => {
+        db.addEventListener("click", () => { deleteItem(i) });
+    });
+}
+
+function activateEditListeners() {
+    const editBtn = document.querySelectorAll(".editBtn");
+    const updatecontroller = document.querySelectorAll(".update-controller");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    editBtn.forEach((eb, i) => {
+        eb.addEventListener("click", () => {
+            updatecontroller[i].style.display = "block";
+            inputs[i].disabled = false;
+        });
+    });
+}
+
+function activateSaveListeners() {
+    const saveBtn = document.querySelectorAll(".saveBtn");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    saveBtn.forEach((sb, i) => {
+        sb.addEventListener("click", () => {
+            updateItem(inputs[i].value, i);
+        });
+    });
+}
+
+function activateCancelListeners() {
+    const cancelBtn = document.querySelectorAll(".cancelBtn");
+    const updatecontroller = document.querySelectorAll(".input-controller");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    cancelBtn.forEach((cb, i) => {
+        cb.addEventListener("click", () => {
+            updatecontroller[i].style.display = "none";
+            inputs[i].disabled = true;
+        });
+    });
+}
+
+function deleteItem(i) {
+    itemsArray.splice(i, 1);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    location.reload();
+}
+
+function createItem(itemInput) {
+    itemsArray.push(itemInput.value);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    location.reload();
+}
+
+function updateItem(text, i) {
+    itemsArray[i] = text;
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    location.reload();
+}
+
+function displayDate() {
+    let date = new Date();
+    date = date.toString().split(" ");
+    document.querySelector("#Date").innerHTML = date[1] + " " + date[2] + " " + date[3];
+}
+
+window.onload = function () {
+    displayDate();
+    displayItems();
+};
